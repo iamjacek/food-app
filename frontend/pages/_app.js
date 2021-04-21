67e15e107a805d12436d05ffeda6e192298b19e9
iamjacek <jake47s@gmail.com>
2021-04-21 17:56:39 +0100

    add context to layout and app, register form, login-register functions

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,12 +3,53 @@ import App from "next/app"
 import Head from "next/head"
 import Layout from "../components/Layout"
 import withApollo from "../lib/apollo"
+import Cookie from "js-cookie"
+import fetch from "isomorphic-fetch"
+import AppContext from "../context/AppContext"
 
 class FoodApp extends App {
+  state = {
+    user: null,
+  }
+
+  componentDidMount() {
+    // grab token value from cookie
+    const token = Cookie.get("token")
+
+    if (token) {
+      // authenticate the token on the server and place set user object
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}users/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(async (res) => {
+        // if res comes back not valid, token is not valid
+        // delete the token and log the user out on client
+        if (!res.ok) {
+          Cookie.remove("token")
+          this.setState({ user: null })
+          return null
+        }
+        const user = await res.json()
+        this.setUser(user)
+      })
+    }
+  }
+
+  setUser = (user) => {
+    this.setState({ user })
+  }
+
   render() {
     const { Component, props } = this.props
     return (
-      <>
+      <AppContext.Provider
+        value={{
+          user: this.state.user,
+          isAuthenticated: !!this.state.user,
+          setUser: this.setUser,
+        }}
+      >
         <Head>
           <link
             rel="stylesheet"
@@ -17,10 +58,11 @@ class FoodApp extends App {
             crossOrigin="anonymous"
           />
         </Head>
+
         <Layout>
           <Component {...props} />
         </Layout>
-      </>
+      </AppContext.Provider>
     )
   }
 }
